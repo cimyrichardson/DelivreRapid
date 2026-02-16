7be266e0537033f13e3ea9fade31c1cba4657ba3
samuelharrigan2002-sudo <samuelharrigan2002@gmail.com>
2026-02-16 15:41:07 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class DelivreRapidApp extends StatelessWidget {
     return MaterialApp(
       title: 'DelivreRapid',
       debugShowCheckedModeBanner: false,
-      home:const adm(),
+      home:const splash (),
     );
   }
 }
